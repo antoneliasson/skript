f6106ff063abb13b0408c7ee25fa027eaae856c8
Anton Eliasson <devel@antoneliasson.se>
2013-09-17 22:44:10 +0200

    Städat upp och dokumenterat ipmonitor

--- ipmonitor.py
+++ ipmonitor.py
@@ -2,15 +2,41 @@
 """
 IP Monitor
 
-version 1
-
 Sends an e-mail if the network's external IP address has changed since last run.
+
+SMTP credentials are read from the user's ~/.netrc file. It expects an entry like this:
+
+    machine smtp.example.com
+    login johndoe@example.com
+    password supersecret
+
+where `machine` matches the `smtp_address` below and `login` is typically your
+e-mail address with or without the domain depending on your SMTP server.
+
+Typically, this program is run as a cron job by a crontab entry like this:
+
+    0 */12 * * * ~/bin/ipmonitor.py
+
+(Every day, at 0:00 and 12:00 in this example)
+
 """
 
-def get_ip():
-    import urllib.request
-    import re
+# Configuration
+smtp_address = 'mail.antoneliasson.se'
+sender_address = 'relay@antoneliasson.se' # E-mail 'From' field
+recipient_address = 'info@antoneliasson.se' # E-mail 'To' field
 
+import urllib.request
+import re
+import os.path
+
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+from netrc import netrc
+from smtplib import SMTP
+
+def get_ip():
     with urllib.request.urlopen('http://dns.loopia.se/checkip/checkip.php') as f:
         html = f.read().decode('utf-8')
         ip = re.findall('[0-9]+.[0-9]+.[0-9]+.[0-9]+', html)[0]
@@ -18,8 +44,7 @@ def get_ip():
     return ip
 
 def compare(ip):
-    from os.path import expanduser
-    home = expanduser("~")
+    home = os.path.expanduser("~")
     try:
         file = open(home + '/.ipmonitor', 'r')
         return file.read() == ip
@@ -27,8 +52,7 @@ def compare(ip):
         return False
 
 def update(ip):
-    from os.path import expanduser
-    home = expanduser("~")
+    home = os.path.expanduser("~")
     try:
         file = open(home + '/.ipmonitor', 'w')
         file.write(ip)
@@ -36,50 +60,42 @@ def update(ip):
     except IOError as e:
         return False
 
-def compose(ip, wrote, poster, recipient):
-    from email.mime.text import MIMEText
-    from email.utils import formatdate
-    
+def compose(ip, wrote, sender, recipient):
     msg = MIMEText('')
     if wrote:
         msg.set_payload('New IP address: %s.' % ip)
     else:
         msg.set_payload('New IP address: %s.\n\nUnable to create config file!' % ip)
     msg['Subject'] = 'IP address change: %s' % ip
-    msg['From'] = poster
+    msg['From'] = sender
     msg['To'] = recipient
-    # RFC 2822: "Use local time". Anton: "Use UTC".
+    # RFC 2822, section 3.3: "The date and time-of-day SHOULD express local time."
+    # Anton: "Use UTC and the world will be a better place."
     msg['Date'] = formatdate(localtime = False)
     
     return msg
 
 def send(msg, server, port):
-    from netrc import netrc
-    from smtplib import SMTP
-    
     username, _, password = netrc().authenticators(server)
     
     smtp = SMTP(server, port)
     smtp.starttls()
     smtp.login(username, password)
-    # smtp.send_message(msg) # Python 3.2
-    smtp.sendmail(msg['From'], msg['To'], str(msg)) # Python 3.1
+    smtp.send_message(msg) # Python >= 3.2
+    # smtp.sendmail(msg['From'], msg['To'], str(msg)) # Python >= 3.1
     smtp.quit()
 
 def main():
     ip = get_ip()
-    if compare(ip):
-        pass
-#        print('No change')
-    else:
+    if not compare(ip):
         print('New IP: %s' % ip)
         wrote = update(ip)
         if wrote:
             print('Wrote config')
         else:
             print('Failed to write config')
-        msg = compose(ip, wrote, 'relay@antoneliasson.se', 'info@antoneliasson.se')
-        send(msg, 'mail.antoneliasson.se', 587)
+        msg = compose(ip, wrote, sender_address, recipient_address)
+        send(msg, smtp_address, 587)
         print('Sent e-mail')
 
 if __name__ == '__main__':
